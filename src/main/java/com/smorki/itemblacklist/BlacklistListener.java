5ee567790e48ae95c710beb06c6c676904c9300a
Smorki <www.emir20111210@gmail.com>
2026-08-13 20:12:55 +0300

    Remove shadow plugin and add right-click place check

--- src/main/java/com/smorki/itemblacklist/BlacklistListener.java
+++ src/main/java/com/smorki/itemblacklist/BlacklistListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -63,6 +64,10 @@ public final class BlacklistListener implements Listener {
         Player player = event.getPlayer();
 
         if (hasBypass(player)) {
+            return;
+        }
+
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && !plugin.isBlockPlace()) {
             return;
         }
 
